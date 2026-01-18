fbb04c2c952a7ae9349a2a5c43be5696ea5f05f1
Lukáš Mikula <mikula.luky@gmail.com>
2026-01-18 20:30:53 +0100

    sourcing characters and images from APIs typesafly

--- README.md
+++ README.md
@@ -5,6 +5,10 @@ A small journal of all people in Star Wars movies.
 - Data source: [SWAPI](https://swapi.py4e.com/)
 - Image source: [https://github.com/vieraboschkova/swapi-gallery](https://github.com/vieraboschkova/swapi-gallery)
 
+## Pinned Decisions
+
+- **May the Force of SSG be with you.** The character list is ~99% immutable. This data likely only changes with a new Star Wars release, and even then, API updates are not guaranteed. By fetching at build-time, I’ve prioritized performance boost of SSG over the slim risk of stale data, which might happen years down the line. Given the velocity of the React ecosystem, it is far more likely that dependency maintenance will be required before the data itself becomes outdated.
+
 ## Run it locally
 
 Run the development server:
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,41 @@
-import { clsx, type ClassValue } from 'clsx';
-import { twMerge } from 'tailwind-merge';
+import { Character, SWAPICharacterRaw } from "@/types/types";
+import axios from "axios";
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-	return twMerge(clsx(inputs));
+  return twMerge(clsx(inputs));
+}
+
+export function getIdFromUrl(url: string) {
+  const matches = url.match(/\/(\d+)\/$/);
+  return Number(matches ? matches[1] : "");
+}
+
+export async function getPeople(url: string = ""): Promise<Character[]> {
+  if (!url) throw new Error("No URL was provided.");
+
+  const firstPage = await axios.get(url);
+  const totalCount = firstPage.data.count;
+  const totalPages = Math.ceil(totalCount / 10);
+
+  const pagePromises = [];
+  for (let i = 2; i <= totalPages; i++) {
+    pagePromises.push(axios.get(`${url}?page=${i}`));
+  }
+
+  const remainingPages = await Promise.all(pagePromises);
+
+  const allResults = [
+    ...firstPage.data.results,
+    ...remainingPages.flatMap((res) => res.data.results),
+  ];
+
+  return allResults.map(
+    (char: SWAPICharacterRaw) =>
+      ({
+        id: getIdFromUrl(char.url),
+        ...char,
+      }) as Character,
+  );
 }
