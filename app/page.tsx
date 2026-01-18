68069d00c1e91a830046d5e02c85f50831d0b717
Lukáš Mikula <mikula.luky@gmail.com>
2026-01-18 22:24:04 +0100

    [refactor] rename getPeople() to getCharacters() and enforce proper project structure

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
 import PeopleList from "@/components/PeopleList";
-import { getPeople } from "@/lib/utils";
+import { getCharacters } from "@/services/characters.service";
 import { Character } from "@/types/types";
 
 export default async function Home() {
-  const peopleArr: Character[] = await getPeople(process.env.DATA_URL);
+  const peopleArr: Character[] = await getCharacters(process.env.DATA_URL);
 
   return (
     <div className="">
---
+++ services/characters.service.ts
@@ -0,0 +1,31 @@
+import { getIdFromUrl } from "@/lib/utils";
+import { Character, SWAPICharacterRaw } from "@/types/types";
+import axios from "axios";
+
+export async function getCharacters(url: string = ""): Promise<Character[]> {
+  if (!url) throw new Error("No URL was provided.");
+
+  const firstPage = await axios.get(url);
+  const totalCount = firstPage.data.count;
+  const totalPages = Math.ceil(totalCount / 10);
+
+  const pagePromises = [];
+  for (let i = 2; i <= totalPages; i++) {
+    pagePromises.push(axios.get(`${url}?page=${i}`));
+  }
+
+  const remainingPages = await Promise.all(pagePromises);
+
+  const allResults = [
+    ...firstPage.data.results,
+    ...remainingPages.flatMap((res) => res.data.results),
+  ];
+
+  return allResults.map(
+    (char: SWAPICharacterRaw) =>
+      ({
+        id: getIdFromUrl(char.url),
+        ...char,
+      }) as Character,
+  );
+}
