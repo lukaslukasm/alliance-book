1082644df70bcf70737635ee31aaa0e80c1254d3
Lukáš Mikula <mikula.luky@gmail.com>
2026-01-19 11:57:58 +0100

    [hotfix] adjust env url name for image sources in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    remotePatterns: [new URL(`${process.env.IMG_URL}**`)],
+    remotePatterns: [new URL(`${process.env.NEXT_PUBLIC_IMG_URL}**`)],
   },
 };
 
