c81b5fffed6f35a62faf13524fc69205a8de65f4
Lukáš Mikula <mikula.luky@gmail.com>
2026-01-21 20:41:24 +0100

    [Refactor] move createURL() function into utils

--- components/pagination-component.tsx
+++ components/pagination-component.tsx
@@ -8,6 +8,7 @@ import {
   PaginationNext,
   PaginationPrevious,
 } from "./ui/pagination";
+import { createURL } from "@/lib/utils";
 
 export type PaginationComponentProps = {
   currentPage: number;
@@ -20,55 +21,38 @@ export default function PaginationComponent({
   pageCount,
   searchParams,
 }: PaginationComponentProps) {
-  function createURL(targetPageId: number) {
-    const newURL = new URLSearchParams();
-    const flattenedFilters = Object.entries(searchParams).flatMap(
-      ([key, value]) => {
-        if (Array.isArray(value)) {
-          return value.map((v) => [key, v]);
-        }
-        return [[key, value]];
-      },
-    );
-
-    flattenedFilters.forEach(([key, value]) => {
-      newURL.append(`${key}`, `${value}`);
-    });
-    newURL.set("page", targetPageId.toString());
-
-    return `?${newURL.toString()}`;
-  }
-
   return (
     <Pagination>
       <PaginationContent>
         {currentPage > 1 && (
           <>
             <PaginationItem>
-              <PaginationPrevious href={createURL(currentPage - 1)} />
+              <PaginationPrevious
+                href={createURL(currentPage - 1, searchParams)}
+              />
             </PaginationItem>
             <PaginationItem>
-              <PaginationLink href={createURL(currentPage - 1)}>
+              <PaginationLink href={createURL(currentPage - 1, searchParams)}>
                 {currentPage - 1}
               </PaginationLink>
             </PaginationItem>
           </>
         )}
         <PaginationItem>
-          <PaginationLink href={createURL(currentPage)} isActive>
+          <PaginationLink href={createURL(currentPage, searchParams)} isActive>
             {currentPage}
           </PaginationLink>
         </PaginationItem>
         {currentPage < pageCount && (
           <>
             <PaginationItem>
-              <PaginationLink href={createURL(currentPage + 1)}>
+              <PaginationLink href={createURL(currentPage + 1, searchParams)}>
                 {Number(currentPage + 1)}
               </PaginationLink>
             </PaginationItem>
             {currentPage + 1 < pageCount && <PaginationEllipsis />}
             <PaginationItem>
-              <PaginationNext href={createURL(currentPage + 1)} />
+              <PaginationNext href={createURL(currentPage + 1, searchParams)} />
             </PaginationItem>
           </>
         )}
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,3 +1,4 @@
+import { SearchParams } from "@/types/types";
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
@@ -5,9 +6,36 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
+/**
+ * Retrieves an ID from a given URL.
+ *
+ */
 export function getIdFromUrl(url: string) {
   if (!url) throw new Error(`${url} is not a URL.`);
 
   const matches = url.match(/\/(\d+)\/$/);
   return Number(matches ? matches[1] : "");
+}
+
+/**
+ * Creates a new URL with a page param pointing to targetPageId while honouring existing URL params.
+ *
+ */
+export function createURL(targetPageId: number, searchParams: SearchParams) {
+  const newURL = new URLSearchParams();
+  const flattenedFilters = Object.entries(searchParams).flatMap(
+    ([key, value]) => {
+      if (Array.isArray(value)) {
+        return value.map((v) => [key, v]);
+      }
+      return [[key, value]];
+    },
+  );
+
+  flattenedFilters.forEach(([key, value]) => {
+    newURL.append(`${key}`, `${value}`);
+  });
+  newURL.set("page", targetPageId.toString());
+
+  return `?${newURL.toString()}`;
 }
